32e7b17360499418d1bf1de0675e549adff8d8b0
Martin Schenck <martin.schenck@futurice.com>
2020-09-16 15:17:02 +0200

    Small changes to rofi and polybar

--- .config/i3/maintenance.sh
+++ .config/i3/maintenance.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-command=$(printf "Display settings:\tarandr\nAudio settings:  \tpavucontrol\nRescan audio devices:\trm -rf ~/.config/pulse && pulseaudio -k\nSelect wifi:\tkitty -e nmtui-connect" | rofi -dmenu -font "DejaVuSansMono Nerd Font 10" -theme sidetab_nord | awk 'BEGIN { FS="\t" } ; { print $2 }')
+command=$(printf "Display settings:\tarandr\nAudio settings:  \tpavucontrol\nRescan audio devices:\trm -rf ~/.config/pulse && pulseaudio -k\nSelect wifi:        \tkitty -e nmtui-connect" | rofi -dmenu -font "DejaVuSansMono Nerd Font 10" -theme slate_nord | awk 'BEGIN { FS="\t" } ; { print $2 }')
 eval $command
