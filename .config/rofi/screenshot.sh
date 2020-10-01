297099148cf06260650cd0dae800310d4ae1f6ce
Martin Schenck <martin.schenck@futurice.com>
2020-10-01 18:31:45 +0200

    Screenshots a la rofi

---
+++ .config/rofi/screenshot.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+screenshotname="$(xdg-user-dir PICTURES)/$(date +'%s_screenshot.png')"
+
+# Options
+screen="Entire screen"
+area="Area or window"
+area_clip="Area or window to clipboard"
+
+options="${screen}\n${area}\n${area_clip}"
+
+selected="$(echo -e ${options} | rofi -dmenu -i -p ' ' -theme slate_nord)"
+
+case $selected in
+    $screen)
+        maim $screenshotname
+        ;;
+    $area)
+        maim -s $screenshotname
+        ;;
+    $area_clip)
+        maim -s | xclip -selection clipboard -t image/png
+        ;;
+esac
