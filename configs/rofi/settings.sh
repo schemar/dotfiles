808c8685cc823e0411ed57cfffb3780b8d3d56ad
Martin Schenck <martin.schenck@futurice.com>
2021-06-11 21:08:20 +0200

    Revive i3-only

--- configs/rofi/settings.sh
+++ configs/rofi/settings.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
 # Options
-gnome_settings=" Gnome settings"
 display=" Display"
 audio="蓼 Audio"
 audio_rescan="遼 Rescan audio devices"
-keyboard_reload=" Reload keyboard config"
 network=" Network"
 
-options="${gnome_settings}\n${network}\n${display}\n${audio}\n${audio_rescan}\n${keyboard_reload}"
+options="${network}\n${display}\n${audio}\n${audio_rescan}"
 
 selected="$(echo -e ${options} | rofi -dmenu -i -p ' ' -theme slate_nord)"
 
 case $selected in
-    $gnome_settings)
-        gnome-control-center
-        ;;
     $display)
         arandr
         ;;
@@ -24,9 +19,6 @@ case $selected in
         ;;
     $audio_rescan)
         rm -rf ~/.config/pulse && pulseaudio -k
-        ;;
-    $keyboard_reload)
-        setxkbmap -layout us  -option 'compose:menu' -option 'ctrl:nocaps' -variant altgr-intl && xset r rate 300 40
         ;;
     $network)
         kitty --title "settings-nmtui-connect" nmtui-connect
