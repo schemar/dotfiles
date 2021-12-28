833dc7ad61a330bf2bab1344d3004ad6efb932a2
Martin Schenck <martin.schenck@futurice.com>
2021-12-28 22:04:32 +0100

    Define todo keywords for org mode

--- configs/emacs/init.el
+++ configs/emacs/init.el
@@ -4,7 +4,28 @@
 ;; Define the init file
 
 ;;; Code:
-;; We're actually loading configuration from init.org.
+     (setq package-enable-at-startup nil)
+     (defvar bootstrap-version)
+     (let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+       (bootstrap-version 5))
+       (unless (file-exists-p bootstrap-file)
+	 (with-current-buffer
+	     (url-retrieve-synchronously
+	      "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	      'silent 'inhibit-cookies)
+	   (goto-char (point-max))
+	   (eval-print-last-sexp)))
+       (load bootstrap-file nil 'nomessage))
+     (straight-use-package 'use-package)
+     (setq straight-use-package-by-default t)
+
+     (use-package org
+       :init
+       (require 'org))
+;; We're actually loading configuration from settings.org.
+;; This has to be done after the org package has been installed in order to load
+;; the latest version instead of the built-in one.
 (require 'org)
 (org-babel-load-file
   (expand-file-name "settings.org"
