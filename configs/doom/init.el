6001a0d87d1624d0b0be5a7ab769fc5c5104d240
Martin Schenck <martin.schenck@futurice.com>
2022-02-23 17:54:13 +0100

    Doom: tune ivy

--- configs/doom/init.el
+++ configs/doom/init.el
@@ -23,7 +23,7 @@
        (company +childframe) ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +fuzzy +childframe +icons) ; a search engine for love and life
+       (ivy +prescient +icons) ; a search engine for love and life
        ;;(vertico +icons)    ; the search engine of the future
 
        :ui
