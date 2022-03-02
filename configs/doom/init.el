d80b3e48c15e2751e611a2d836dd371a56df13bb
Martin Schenck <martin.schenck@futurice.com>
2022-03-02 18:17:10 +0100

    doom: explicitly set aspell

--- configs/doom/init.el
+++ configs/doom/init.el
@@ -80,7 +80,7 @@
 
        :checkers
        (syntax +childframe) ; tasing you for every semicolon you forget
-       (spell +flyspell +everywhere) ; tasing you for misspelling mispelling
+       (spell +flyspell +aspell +everywhere) ; tasing you for misspelling mispelling
        grammar           ; tasing grammar mistake every you make
 
        :tools
