bd629572a53e59ab870f196ae3ff40ba2976028b
Martin Schenck <martin.schenck@futurice.com>
2022-04-14 16:32:30 +0200

    i3 lock: Scale image with black pading

--- configs/i3/lock.sh
+++ configs/i3/lock.sh
@@ -5,8 +5,8 @@ fehbg=$(grep -o "'.*'" ~/.fehbg | sed "s/'//g")
 
 # Scale it to match the screen dimensions.
 tmp_file=$(mktemp -t XXXXXXXXXX.png)
-screen_width="$(xdpyinfo | grep dimensions | cut -d' ' -f7 | cut -dx -f1)"
-convert "$fehbg" -scale "$screen_width" "$tmp_file"
+screen_dimensions="$(xdpyinfo | grep dimensions | cut -d' ' -f7 )"
+convert "$fehbg" -scale "$screen_dimensions" -gravity center -background black -extent "$screen_dimensions" "$tmp_file"
 
 i3lock -i "$tmp_file"
 
