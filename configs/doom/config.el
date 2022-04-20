dcfa0b5d00479a4cb199949db3c605335fa95fe5
Martin Schenck <martin.schenck@futurice.com>
2022-04-20 15:46:23 +0200

    Doom Emacs: Update tree sitter config
    Based on how hlissner set up tree sitter in his config.

--- configs/doom/config.el
+++ configs/doom/config.el
@@ -291,10 +291,21 @@
   :defer t)
 
 ;; Tree Sitter
+;; See also https://github.com/hlissner/doom-emacs-private/blob/master/modules/ui/tree-sitter/config.el
 (use-package! tree-sitter
+  ;; Enable tree sitter for programming modes:
+  :hook (prog-mode . tree-sitter-mode)
+  ;; Syntax highlighting:
+  :hook (tree-sitter-after-on . tree-sitter-hl-mode)
   :config
-  (require 'tree-sitter-langs)
-  (global-tree-sitter-mode)
-
-  ;; Only for syntax highlighting.
-  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+  (require 'tree-sitter-langs)  (defadvice! doom-tree-sitter-fail-gracefully-a (orig-fn &rest args)
+                                  "Don't break with errors when current major mode lacks tree-sitter support."
+                                  :around #'tree-sitter-mode
+                                  (condition-case e
+                                      (apply orig-fn args)
+                                    (error
+                                     (unless (string-match-p (concat "^Cannot find shared library\\|"
+                                                                     "^No language registered\\|"
+                                                                     "cannot open shared object file")
+                                                             (error-message-string e))
+                                       (signal (car e) (cadr e)))))))
