d8fb8ac69e2e8388308f4e537201e51b9f002d34
Martin Schenck <martin.schenck@futurice.com>
2022-05-18 21:18:46 +0200

    Doom: Swap org heading level identifiers

--- configs/doom/config.el
+++ configs/doom/config.el
@@ -195,7 +195,7 @@
 
   ;; Customize the bullet appearance of org headings.
   (setq org-superstar-headline-bullets-list
-        '(9673 9675 9672 9671 9654 9655))
+        '(9672 9671 9673 9675 9654 9655))
 
   ;; Set up keywords incl. when to be asked to add a note.
   (setq org-todo-keywords '((type "TODO(t!)"
