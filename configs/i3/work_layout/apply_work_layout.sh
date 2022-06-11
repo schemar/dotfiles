b712f2bcbb1c407348d6b9ddebb33661f5e06750
Martin Schenck <martin.schenck@futurice.com>
2022-06-11 22:17:42 +0200

    i3: Work layout uses Emacs Clients
    Instead of the "standalone" Emacs window, the clients connect to the
    running daemon of Emacs.
    
    Always connecting to the daemon is consistent. However, it might be
    desirable to have all the main workflows in one Emacs frame with their
    respective workspaces instead of having two Emacs frames on i3
    workspaces 1 and 3.

--- configs/i3/work_layout/apply_work_layout.sh
+++ configs/i3/work_layout/apply_work_layout.sh
@@ -11,10 +11,10 @@ i3-msg "workspace 10; append_layout $SCRIPTPATH/work_layout_10.json"
 
 sleep 1
 
-nohup emacs 1>/dev/null 2>&1 &
+nohup emacsclient -c 1>/dev/null 2>&1 &
 nohup google-chrome 1>/dev/null 2>&1 &
 nohup firefox 1>/dev/null 2>&1 &
-nohup emacs 1>/dev/null 2>&1 &
+nohup emacsclient -c 1>/dev/null 2>&1 &
 nohup slack 1>/dev/null 2>&1 &
 nohup flatpak run com.spotify.Client 2>&1 &
 nohup flatpak run com.discordapp.Discord 2>&1 &
