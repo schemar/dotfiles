0c281c83ef0936b666a8e647080571f3a985a056
Martin Schenck <martin.schenck@futurice.com>
2022-06-23 14:51:00 +0200

    Polybar: Use IPC to manage polybars

--- configs/polybar/launch.sh
+++ configs/polybar/launch.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+polybar-msg cmd quit
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
 # Launch bar(s)
 echo "---" | tee -a /tmp/polybar1.log
-polybar main -c $HOME/.config/polybar/config.ini >>/tmp/polybar1.log 2>&1 & disown
+polybar main -c $HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar_main.log & disown
 
 echo "Bars launched..."
