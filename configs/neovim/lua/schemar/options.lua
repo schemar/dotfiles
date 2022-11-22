bb398865691df8e2a5065b18bce069edf211df3f
Martin Schenck <martin.schenck@afilio.de>
2022-11-22 12:08:08 +0100

    Nvim: Integrate macOS clipboard with yank/paste

--- configs/neovim/lua/schemar/options.lua
+++ configs/neovim/lua/schemar/options.lua
@@ -3,20 +3,21 @@
 local opt = vim.opt
 
 -- [[ Misc ]]
-opt.timeoutlen = 300 -- num:  Timeout, e.g. for which-key
+opt.timeoutlen = 300 -- num: Timeout, e.g. for which-key
+opt.clipboard = 'unnamedplus' -- str: Clipboard integration with macOS
 
 -- [[ Context ]]
-opt.colorcolumn = '80' -- str:  Show col for max line length
+opt.colorcolumn = '80' -- str: Show col for max line length
 opt.number = true -- bool: Show line numbers
-opt.scrolloff = 10 -- int:  Min num lines of context
-opt.signcolumn = 'yes' -- str:  Show the sign column
+opt.scrolloff = 10 -- int: Min num lines of context
+opt.signcolumn = 'yes' -- str: Show the sign column
 
 -- [[ Filetypes ]]
-opt.encoding = 'utf8' -- str:  String encoding to use
-opt.fileencoding = 'utf8' -- str:  File encoding to use
+opt.encoding = 'utf8' -- str: String encoding to use
+opt.fileencoding = 'utf8' -- str: File encoding to use
 
 -- [[ Theme ]]
-opt.syntax = 'ON' -- str:  Allow syntax highlighting
+opt.syntax = 'ON' -- str: Allow syntax highlighting
 opt.termguicolors = true -- bool: If term supports ui color then enable
 opt.cursorline = true -- bool: Highlight current line
 opt.listchars = 'space:·,tab:>~,trail:~,extends:>,precedes:<'
@@ -30,9 +31,9 @@ opt.hlsearch = true -- bool: Highlight search matches
 
 -- [[ Whitespace ]]
 opt.expandtab = true -- bool: Use spaces instead of tabs
-opt.shiftwidth = 2 -- num:  Size of an indent
-opt.softtabstop = 2 -- num:  Number of spaces tabs count for in insert mode
-opt.tabstop = 2 -- num:  Number of spaces tabs count for
+opt.shiftwidth = 2 -- num: Size of an indent
+opt.softtabstop = 2 -- num: Number of spaces tabs count for in insert mode
+opt.tabstop = 2 -- num: Number of spaces tabs count for
 
 -- [[ Splits ]]
 opt.splitright = true -- bool: Place new window to right of current one
