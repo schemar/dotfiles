90fbe072545ce627fbe68cd94a8ef52809124338
Martin Schenck <martin.schenck@afilio.de>
2023-02-10 13:05:03 +0100

    Nvim: Disable fold column for certain filetypes

--- configs/neovim/lua/schemar/plugins/configs/nvim-ufo.lua
+++ configs/neovim/lua/schemar/plugins/configs/nvim-ufo.lua
@@ -8,6 +8,18 @@ return {
 	},
 	config = function(_, opts)
 		vim.o.foldcolumn = "1" -- '0' is not bad
+		-- Disable the fold column for certain filetypes:
+		vim.api.nvim_create_autocmd("Filetype", {
+			pattern = {
+				"aerial",
+				"gitcommit",
+				"NvimTree",
+				"NeogitCommitMessage",
+				"NeogitStatus",
+			},
+			command = "set foldcolumn=0",
+		})
+
 		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 		vim.o.foldlevelstart = 99
 		vim.o.foldenable = true
