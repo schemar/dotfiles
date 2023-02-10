b578e6d61a807a326673c5d1c1b56d681b67d42f
Martin Schenck <martin.schenck@afilio.de>
2023-02-10 11:43:32 +0100

    Nvim: Add nvim-ufo for folds

---
+++ configs/neovim/lua/schemar/plugins/configs/nvim-ufo.lua
@@ -0,0 +1,21 @@
+return {
+	"kevinhwang91/nvim-ufo", -- Better folding
+	name = "ufo",
+	dependencies = { "kevinhwang91/promise-async" },
+	priority = 1, -- Load after LSP, etc.
+	opts = {
+		close_fold_kinds = { "imports" },
+	},
+	config = function(_, opts)
+		vim.o.foldcolumn = "1" -- '0' is not bad
+		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+
+		-- Must be overridden in order for ufo to keep its folds
+		vim.keymap.set("n", "zR", require("ufo").openFoldsExceptKinds)
+		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+
+		require("ufo").setup(opts)
+	end,
+}
