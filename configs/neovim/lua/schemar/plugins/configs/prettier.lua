1cbfa9ab01dd1fe399302d4f8aa59b9df00f4363
Martin Schenck <martin.schenck@afilio.de>
2023-02-14 20:43:40 +0100

    Nvim: Add events to prettier config

--- configs/neovim/lua/schemar/plugins/configs/prettier.lua
+++ configs/neovim/lua/schemar/plugins/configs/prettier.lua
@@ -1,5 +1,6 @@
 return {
 	"MunifTanjim/prettier.nvim", -- Prettier for TS/JS formatting (for null-ls)
+	event = { "BufReadPre", "BufNewFile" },
 	opts = {
 		bin = "prettierd", -- or `"prettier"`
 		filetypes = {
