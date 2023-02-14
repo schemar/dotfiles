f8b6fcda6be757f18e83f052d05bc9a8fc5e9d0a
Martin Schenck <martin.schenck@afilio.de>
2023-02-14 21:55:00 +0100

    Nvim: Move simple configs into declarations

--- configs/neovim/init.lua
+++ configs/neovim/init.lua
@@ -12,3 +12,19 @@ vim.g.localleader = ","
 require("schemar.options")
 require("schemar.plugins")
 require("schemar.keymaps")
+
+--
+-- Borders
+local _border = "single"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+	border = _border,
+})
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+	border = _border,
+})
+
+vim.diagnostic.config({
+	float = { border = _border },
+})
--- configs/neovim/lua/schemar/plugins/configs/init.lua
+++ configs/neovim/lua/schemar/plugins/configs/init.lua
@@ -5,12 +5,43 @@ return {
 
 	-- [[ Theming ]]
 	"mrjones2014/nvim-ts-rainbow", -- Rainbow parentheses
-	"lewis6991/gitsigns.nvim", -- Git gutter
+	{
+		"lewis6991/gitsigns.nvim", -- Git gutter
+		event = { "BufReadPre", "BufNewFile" },
+		config = true,
+	},
 
-	"folke/todo-comments.nvim", -- Highlight and list TODOs, etc.
-	"norcalli/nvim-colorizer.lua", -- Show color-codes colored
+	{
+		"folke/todo-comments.nvim", -- Highlight and list TODOs, etc.
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			highlight = {
+				pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlightng (vim regex)
+				-- Was [[.*<(KEYWORDS)\s*:]] including colon.
+			},
+			search = {
+				pattern = [[\b(KEYWORDS)]], -- ripgrep regex
+				-- Was [[\b(KEYWORDS):]] including colon.
+			},
+		},
+	},
+	{
+		"norcalli/nvim-colorizer.lua", -- Show color-codes colored
+		event = { "BufReadPre", "BufNewFile" },
+		config = true,
+	},
 	"danilamihailov/beacon.nvim", -- Highlight cursor on jump
-	"nvim-tree/nvim-web-devicons", -- Fancy icons in pop-ups
+	{
+		"nvim-tree/nvim-web-devicons", -- Fancy icons in pop-ups
+		opts = {
+			-- globally enable different highlight colors per icon (default to true)
+			-- if set to false all icons will have the default icon's color
+			color_icons = true,
+			-- globally enable default icons (default to false)
+			-- will get overriden by `get_icons` option
+			default = true,
+		},
+	},
 	"onsails/lspkind.nvim", -- Icons in completion dialogue
 
 	-- [[ Interface ]]
@@ -19,15 +50,49 @@ return {
 	{
 		"kylechui/nvim-surround", -- E.g. cs"' to replace surrounding " with '
 		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = { "BufReadPre", "BufNewFile" },
+		config = true,
 	},
-	"folke/which-key.nvim", -- Like Emacs which key
+	{
+		"folke/which-key.nvim", -- Like Emacs which key
+		config = true,
+	},
 
-	"numToStr/Comment.nvim", -- Easier (un)commenting
+	{
+		"numToStr/Comment.nvim", -- Easier (un)commenting
+		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			pre_hook = function()
+				return require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+			end,
+		},
+	},
 	"JoosepAlviste/nvim-ts-context-commentstring", -- Improved comment management; integrates with Comment.nvim
 
-	"booperlv/nvim-gomove", -- Alt-h/j/k/l to move line
+	{
+		"booperlv/nvim-gomove", -- Alt-h/j/k/l to move line
+		event = { "BufReadPre", "BufNewFile" },
+		config = true,
+	},
 	"famiu/bufdelete.nvim", -- Keep windows around when deleting buffers
-	"rgroli/other.nvim", -- Go to alternative file, e.g. ts<->vue or test
+	{
+		"rgroli/other.nvim", -- Go to alternative file, e.g. ts<->vue or test
+		name = "other-nvim",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			-- Map files to other files.
+			-- See documentation for details and more options.
+			mappings = {
+				{ pattern = "(.*)/(.*).ts", target = "%1/%2.vue", context = "vue" },
+				{ pattern = "(.*)/(.*).vue", target = "%1/%2.ts", context = "vue" },
+				{ pattern = "(.*)/(.*).ts", target = "%1/%2.spec.ts", context = "spec" },
+				{ pattern = "(.*)/(.*).spec.ts", target = "%1/%2.ts", context = "spec" },
+				{ pattern = "(.*)/(.*).ts", target = "%1/%2.test.ts", context = "test" },
+				{ pattern = "(.*)/(.*).test.ts", target = "%1/%2.ts", context = "test" },
+			},
+		},
+	},
 
 	-- [[ Languages ]]
 	"neovim/nvim-lspconfig", -- Configurations for Nvim LSP
@@ -38,17 +103,21 @@ return {
 	"williamboman/mason-lspconfig.nvim", -- Integration mason/lsp
 	"jayp0521/mason-null-ls.nvim", -- Integration mason/null-ls
 
-	"ThePrimeagen/refactoring.nvim", -- Refactoring tools for code
 	"SmiteshP/nvim-navic", -- Winbar breadcrumbs, e.g. for code context
 
 	"b0o/schemastore.nvim", -- Schemas for JSON files
 
-	"windwp/nvim-autopairs", -- Auto-pair tags, etc.
+	{
+		"windwp/nvim-autopairs", -- Auto-pair tags, etc.
+		event = { "BufReadPre", "BufNewFile" },
+		config = true,
+	},
 	"windwp/nvim-ts-autotag", -- Auto-tags for HTML, Vue, etc.
 
 	-- Treesitter
 	{
 		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPost", "BufNewFile" },
 		build = function()
 			local ts_update = require("nvim-treesitter.install").update({
 				with_sync = true,
