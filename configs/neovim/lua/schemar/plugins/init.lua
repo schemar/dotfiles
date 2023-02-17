21b2dd198443680708523f2b62d1662147c590c5
Martin Schenck <martin.schenck@afilio.de>
2023-02-17 17:37:42 +0100

    Nvim: Add icons for test files

--- configs/neovim/lua/schemar/plugins/init.lua
+++ configs/neovim/lua/schemar/plugins/init.lua
@@ -50,6 +50,18 @@ return {
 			-- globally enable default icons (default to false)
 			-- will get overriden by `get_icons` option
 			default = true,
+			override = {
+				["unit.test.ts"] = {
+					icon = "󰙨",
+					color = "#3178c6",
+					name = "TestTs",
+				},
+				["firestore.test.ts"] = {
+					icon = "󰙨",
+					color = "#3178c6",
+					name = "TestTs",
+				},
+			},
 		},
 	},
 
