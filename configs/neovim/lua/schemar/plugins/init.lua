7a32fbe4e62dcf479fc3b66a643fedeea804be46
Martin Schenck <martin.schenck@afilio.de>
2023-02-17 17:48:31 +0100

    Nvim: Add test files to web icons

--- configs/neovim/lua/schemar/plugins/init.lua
+++ configs/neovim/lua/schemar/plugins/init.lua
@@ -41,29 +41,6 @@ return {
 		"danilamihailov/beacon.nvim", -- Highlight cursor on jump
 		event = { "BufReadPost", "BufNewFile" },
 	},
-	{
-		"nvim-tree/nvim-web-devicons", -- Fancy icons in pop-ups
-		opts = {
-			-- globally enable different highlight colors per icon (default to true)
-			-- if set to false all icons will have the default icon's color
-			color_icons = true,
-			-- globally enable default icons (default to false)
-			-- will get overriden by `get_icons` option
-			default = true,
-			override = {
-				["unit.test.ts"] = {
-					icon = "󰙨",
-					color = "#3178c6",
-					name = "TestTs",
-				},
-				["firestore.test.ts"] = {
-					icon = "󰙨",
-					color = "#3178c6",
-					name = "TestTs",
-				},
-			},
-		},
-	},
 
 	-- [[ Interface ]]
 	{
