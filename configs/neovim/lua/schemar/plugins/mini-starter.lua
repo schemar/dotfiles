0b2aeb8ceda8d1ab15579ed0fa2d6169d0afb46f
Martin Schenck <martin.schenck@afilio.de>
2023-02-23 13:16:42 +0100

    Nvim: Configure mini.starter

--- configs/neovim/lua/schemar/plugins/mini-starter.lua
+++ configs/neovim/lua/schemar/plugins/mini-starter.lua
@@ -3,6 +3,22 @@ return {
 	version = false,
 	event = "VimEnter",
 	config = function()
-		require("mini.starter").setup()
+		local starter = require("mini.starter")
+		starter.setup({
+			evaluate_single = false,
+			items = {
+				starter.sections.recent_files(10, true),
+				starter.sections.recent_files(10, false),
+				starter.sections.builtin_actions(),
+				-- Enable when mini.sessions is set up
+				-- starter.sections.sessions(5, true),
+			},
+			content_hooks = {
+				starter.gen_hook.adding_bullet(),
+				starter.gen_hook.aligning("center", "top"),
+				starter.gen_hook.padding(0, 5),
+			},
+			footer = "\n🚀 Rocket Science",
+		})
 	end,
 }
