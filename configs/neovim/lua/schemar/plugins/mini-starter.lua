3cc9e203e1a9117caa0853c82d23a39f9c35e37c
Martin Schenck <martin.schenck@afilio.de>
2023-03-08 22:08:26 +0100

    Nvim: Starter: Use <C-j/k>

--- configs/neovim/lua/schemar/plugins/mini-starter.lua
+++ configs/neovim/lua/schemar/plugins/mini-starter.lua
@@ -18,7 +18,38 @@ return {
 				starter.gen_hook.aligning("center", "top"),
 				starter.gen_hook.padding(0, 5),
 			},
-			footer = "\n🚀 Rocket Science",
+			header = table.concat({
+				" ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
+				" ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
+				" ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
+				" ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
+				" ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
+				" ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+			}, "\n"),
+			footer = function()
+				return "🚀 Rocket science since " .. os.date("%Y-%m-%dT%H:%M:%S")
+			end,
+		})
+
+		-- Use <C-j/k> to move cursor:
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "MiniStarterOpened",
+			callback = function(opts)
+				vim.api.nvim_buf_set_keymap(
+					opts.buf,
+					"n",
+					"<C-j>",
+					"<Cmd>lua MiniStarter.update_current_item('next')<CR>",
+					{ nowait = true, silent = true }
+				)
+				vim.api.nvim_buf_set_keymap(
+					opts.buf,
+					"n",
+					"<C-k>",
+					"<Cmd>lua MiniStarter.update_current_item('prev')<CR>",
+					{ nowait = true, silent = true }
+				)
+			end,
 		})
 	end,
 }
