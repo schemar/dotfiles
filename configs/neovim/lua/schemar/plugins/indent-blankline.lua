0e5735532e4a446aa332fc583f8642b423f7ba03
Martin Schenck <martin.schenck@afilio.de>
2023-03-12 21:48:19 +0100

    Nvim: indent-blankline: fix highlighting

--- configs/neovim/lua/schemar/plugins/indent-blankline.lua
+++ configs/neovim/lua/schemar/plugins/indent-blankline.lua
@@ -2,19 +2,12 @@ return {
 	"lukas-reineke/indent-blankline.nvim",
 	event = { "BufReadPost", "BufNewFile" },
 	opts = {
-		-- char = "▏",
-		char = "│",
+		char_list = { "│", "¦", "┆", "┊" },
 		filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
 		show_trailing_blankline_indent = false,
 		show_current_context = true,
-		show_current_context_start = true,
-		char_highlight_list = {
-			"IndentBlanklineIndent1",
-			"IndentBlanklineIndent2",
-			"IndentBlanklineIndent3",
-			"IndentBlanklineIndent4",
-			"IndentBlanklineIndent5",
-			"IndentBlanklineIndent6",
-		},
+		show_current_context_start = false, -- If true, underline context start (with treesitter)
+		show_end_of_line = false, -- On empty lines
+		space_char_blankline = " ",
 	},
 }
