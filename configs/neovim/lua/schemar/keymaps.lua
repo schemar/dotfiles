3c376996143c79f52d60808195ac3a8af004701b
Martin Schenck <martin.schenck@afilio.de>
2023-03-16 14:34:14 +0100

    Nvim: Add keymap for tab split

--- configs/neovim/lua/schemar/keymaps.lua
+++ configs/neovim/lua/schemar/keymaps.lua
@@ -5,41 +5,42 @@ local wk = require("which-key")
 
 -- Without prefix:
 wk.register({
-  ["<c-w>"] = {
-    name = "Windows",
-    s = { ":vsplit<CR>", "Split window" },
-    v = { ":split<CR>", "Split window vertically" },
-  },
+	["<c-w>"] = {
+		name = "Windows",
+		s = { ":vsplit<CR>", "Split window" },
+		v = { ":split<CR>", "Split window vertically" },
+	},
 })
 
 -- With leader prefix:
 wk.register({
-  b = {
-    name = "Buffer",
-    n = { ":bn<CR>", "Next buffer" },
-    p = { ":bp<CR>", "Previous buffer" },
-  },
-  f = {
-    name = "File",
-    o = { ":Other<CR>", "Open other file" },
-    O = { ":OtherVSplit<CR>", "Open other file in split" },
-    s = { ":w<CR>", "Save" },
-    S = { ":wa<CR>", "Save all" },
-  },
-  t = {
-    name = "Tab",
-    c = { ":tabclose<CR>", "Close tab" },
-    n = { ":tabnext<CR>", "Next tab" },
-    p = { ":tabprevious<CR>", "Previous tab" },
-    t = { ":tabnew<CR>", "New tab" },
-  },
-  w = {
-    name = "Window",
-    c = { ":close<CR>", "Close window" },
-    s = { ":vsplit<CR>", "Split window" },
-    v = { ":split<CR>", "Vertically split window" },
-  },
-  q = { name = "Quit", q = { ":quitall<CR>", "Quit all" } },
+	b = {
+		name = "Buffer",
+		n = { ":bn<CR>", "Next buffer" },
+		p = { ":bp<CR>", "Previous buffer" },
+	},
+	f = {
+		name = "File",
+		o = { ":Other<CR>", "Open other file" },
+		O = { ":OtherVSplit<CR>", "Open other file in split" },
+		s = { ":w<CR>", "Save" },
+		S = { ":wa<CR>", "Save all" },
+	},
+	t = {
+		name = "Tab",
+		c = { ":tabclose<CR>", "Close tab" },
+		n = { ":tabnext<CR>", "Next tab" },
+		p = { ":tabprevious<CR>", "Previous tab" },
+		s = { ":tab split<CR>", "Split tab with current buffer" },
+		t = { ":tabnew<CR>", "New tab" },
+	},
+	w = {
+		name = "Window",
+		c = { ":close<CR>", "Close window" },
+		s = { ":vsplit<CR>", "Split window" },
+		v = { ":split<CR>", "Vertically split window" },
+	},
+	q = { name = "Quit", q = { ":quitall<CR>", "Quit all" } },
 }, { prefix = "<leader>" })
 
 -- Terminal:
