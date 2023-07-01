0437d622b5b497215e8c6fe72c0d57587db0d798
Martin Schenck <martin.schenck@afilio.de>
2023-07-01 21:21:40 +0200

    Nvim: Add treesitter

--- configs/neovim/lua/schemar/plugins/catppuccin.lua
+++ configs/neovim/lua/schemar/plugins/catppuccin.lua
@@ -1,28 +1,29 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	opts = {
-		integrations = {
-			mason = true,
-			native_lsp = {
-				enabled = true,
-				virtual_text = {
-					errors = { "italic" },
-					hints = { "italic" },
-					warnings = { "italic" },
-					information = { "italic" },
-				},
-				underlines = {
-					errors = { "underline" },
-					hints = { "underline" },
-					warnings = { "underline" },
-					information = { "underline" },
-				},
-				inlay_hints = {
-					background = true,
-				},
-			},
-		}
-	}
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    integrations = {
+      mason = true,
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
+        },
+        underlines = {
+          errors = { "underline" },
+          hints = { "underline" },
+          warnings = { "underline" },
+          information = { "underline" },
+        },
+        inlay_hints = {
+          background = true,
+        },
+      },
+      treesitter = true,
+    }
+  }
 }
