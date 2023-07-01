a3e40f7e8a508b51019a8cb3068efa703b1562ea
Martin Schenck <martin.schenck@afilio.de>
2023-07-01 20:10:41 +0200

    Nvim: Add mason

---
+++ configs/neovim/lua/schemar/plugins/mason-lspconfig.lua
@@ -0,0 +1,8 @@
+return {
+	"williamboman/mason-lspconfig.nvim",
+	lazy = false,
+	dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+	opts = {
+		automatic_installation = true,
+	},
+}
---
+++ configs/neovim/lua/schemar/plugins/mason.lua
@@ -0,0 +1,6 @@
+return {
+	"williamboman/mason.nvim",
+	lazy = false,
+	build = ":MasonUpdate", -- :MasonUpdate updates registry contents
+	config = true,
+}
