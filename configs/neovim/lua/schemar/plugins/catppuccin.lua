a3a96c3328fdbbdef144b2cf7006da31a03066f6
Martin Schenck <martin.schenck@afilio.de>
2023-07-01 21:07:00 +0200

    Nvim: Extend catppuccin

--- configs/neovim/lua/schemar/plugins/catppuccin.lua
+++ configs/neovim/lua/schemar/plugins/catppuccin.lua
@@ -1,1 +1,28 @@
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		integrations = {
+			mason = true,
+			native_lsp = {
+				enabled = true,
+				virtual_text = {
+					errors = { "italic" },
+					hints = { "italic" },
+					warnings = { "italic" },
+					information = { "italic" },
+				},
+				underlines = {
+					errors = { "underline" },
+					hints = { "underline" },
+					warnings = { "underline" },
+					information = { "underline" },
+				},
+				inlay_hints = {
+					background = true,
+				},
+			},
+		}
+	}
+}
