a5bf51c058bab65eadda83b6f699dab3510aa3b5
Martin Schenck <martin.schenck@afilio.de>
2023-08-22 16:50:55 +0200

    Nvim: Replace typescript with typescript-tools

--- configs/neovim/lua/schemar/languages/typescript.lua
+++ configs/neovim/lua/schemar/languages/typescript.lua
@@ -1,15 +1,7 @@
 local lsp_config = require("lspconfig")
 
-require("typescript").setup({
-	disable_commands = false, -- prevent the plugin from creating Vim commands
-	debug = false, -- enable debug logging for commands
-	go_to_source_definition = {
-		fallback = true, -- fall back to standard LSP definition on failure
-	},
-	server = { -- pass options to lspconfig's setup method
-		on_attach = require("schemar.languages.shared").on_attach,
-		capabilities = require("schemar.languages.shared").capabilities,
-	},
+require("typescript-tools").setup({
+	on_attach = require("schemar.languages.shared").on_attach,
 })
 lsp_config.eslint.setup({
 	on_attach = require("schemar.languages.shared").on_attach,
@@ -19,5 +11,4 @@ lsp_config.eslint.setup({
 local null_ls = require("null-ls")
 null_ls.register({
 	null_ls.builtins.formatting.prettier,
-	require("typescript.extensions.null-ls.code-actions"),
 })
--- configs/neovim/lua/schemar/plugins/typescript.lua
+++ configs/neovim/lua/schemar/plugins/typescript.lua
@@ -1,3 +1,4 @@
 return {
-	"jose-elias-alvarez/typescript.nvim",
+	"pmizio/typescript-tools.nvim",
+	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 }
