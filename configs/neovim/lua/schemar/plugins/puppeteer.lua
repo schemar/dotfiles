837eba1afb0a68e4dbe8dd8a27875f80acfc062f
Martin Schenck <martin.schenck@afilio.de>
2023-10-17 10:41:28 +0200

    Nvim: Remove puppeteer
    Too many false-positives

--- configs/neovim/lua/schemar/plugins/puppeteer.lua
+++
@@ -1,6 +0,0 @@
--- Automatically convert from/to template strings based on presence of ${}
-return {
-	"chrisgrieser/nvim-puppeteer",
-	dependencies = "nvim-treesitter/nvim-treesitter",
-	lazy = false, -- plugin lazy-loads itself. Can also load on filetypes.
-}
