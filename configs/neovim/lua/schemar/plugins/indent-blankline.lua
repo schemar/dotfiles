55ebea8da1d38f058c662e99ed3f522d628b6aa7
Martin Schenck <martin.schenck@afilio.de>
2023-10-26 22:23:16 +0200

    Nvim: Remove colors from indent-blankline

--- configs/neovim/lua/schemar/plugins/indent-blankline.lua
+++ configs/neovim/lua/schemar/plugins/indent-blankline.lua
@@ -16,6 +16,7 @@ return {
 		local hooks = require("ibl.hooks")
 		-- create the highlight groups in the highlight setup hook, so they are reset
 		-- every time the colorscheme changes
+		-- TODO: Adapt to catppuccin colors if desired:
 		hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
 			vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
 			vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
@@ -28,8 +29,8 @@ return {
 
 		require("ibl").setup({
 			indent = {
-				highlight = highlight,
-				char = "┊", -- Examples: │ ┃ ┊ ┆ ┇ ┋ ╏
+				-- highlight = highlight,
+				char = "│", -- Examples: │ ┃ ┊ ┆ ┇ ┋ ╏
 			},
 			scope = {
 				enabled = false,
