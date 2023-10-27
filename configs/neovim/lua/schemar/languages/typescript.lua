3d5097d87558d9408adaad781541c48c6514cd08
Martin Schenck <martin.schenck@afilio.de>
2023-10-27 10:30:42 +0200

    Nvim: Remove formatting capability from typescript

--- configs/neovim/lua/schemar/languages/typescript.lua
+++ configs/neovim/lua/schemar/languages/typescript.lua
@@ -1,7 +1,10 @@
 local lsp_config = require("lspconfig")
 
 require("typescript-tools").setup({
-	on_attach = require("schemar.languages.shared").on_attach,
+	on_attach = function(client)
+		-- No formatting from typescript. EFM will handle it.
+		client.resolved_capabilities.document_formatting = false
+	end,
 })
 lsp_config.efm.setup({
 	on_attach = require("schemar.languages.shared").on_attach,
