12615960317e47484aa13ea08d00eedaa7416515
Martin Schenck <martin.schenck@afilio.de>
2024-06-07 13:12:58 +0200

    Nvim: Add "surround"

--- configs/neovim/lua/svim/plugins/editor.lua
+++ configs/neovim/lua/svim/plugins/editor.lua
@@ -312,4 +312,10 @@ return {
 			})
 		end,
 	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = true,
+	},
 }
