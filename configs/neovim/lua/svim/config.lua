77a8dd950b08593687eb32754f66bc0f1e8e93c6
Martin Schenck <martin.schenck@afilio.de>
2024-06-07 12:46:09 +0200

    Nvim: Styling LSP

--- configs/neovim/lua/svim/config.lua
+++ configs/neovim/lua/svim/config.lua
@@ -1,4 +1,4 @@
-return {
+local M = {
 	-- icons used by other plugins
 	icons = {
 		misc = {
@@ -165,3 +165,63 @@ return {
 		},
 	},
 }
+
+-- Options.
+local opt = vim.opt
+
+-- [[ Misc ]]
+opt.timeout = true
+opt.timeoutlen = 300 -- num: Timeout, e.g. for which-key
+opt.updatetime = 1000 -- num: Timeout for "cursor hold" event
+opt.clipboard = "unnamedplus" -- str: Clipboard integration with macOS
+opt.splitkeep = "cursor" -- The default "screen" moves the cursor wrongly, which leads to problems, e.g. with Trouble
+
+-- [[ Context ]]
+opt.colorcolumn = "80" -- str: Show col for max line length
+opt.number = true -- bool: Show line numbers
+opt.scrolloff = 5 -- int: Min num lines of context
+opt.signcolumn = "yes" -- str: Show the sign column
+
+-- [[ Filetypes ]]
+opt.encoding = "utf8" -- str: String encoding to use
+opt.fileencoding = "utf8" -- str: File encoding to use
+
+-- [[ Theme ]]
+opt.syntax = "ON" -- str: Allow syntax highlighting
+opt.termguicolors = true -- bool: If term supports ui color then enable
+opt.cursorline = true -- bool: Highlight current line
+opt.listchars = "space:·,tab:>~,trail:~,extends:>,precedes:<,eol:󰌑"
+-- opt.listchars = "trail:~"
+opt.list = true
+
+-- [[ Search ]]
+opt.ignorecase = true -- bool: Ignore case in search patterns
+opt.smartcase = true -- bool: Override ignorecase if search contains capitals
+opt.incsearch = true -- bool: Use incremental search
+
+-- [[ Whitespace ]]
+opt.expandtab = true -- bool: Use spaces instead of tabs
+opt.shiftwidth = 2 -- num: Size of an indent
+opt.softtabstop = 2 -- num: Number of spaces tabs count for in insert mode
+opt.tabstop = 2 -- num: Number of spaces tabs count for
+
+-- [[ Icons ]]
+local icons = M.icons.diagnostics
+vim.fn.sign_define("DiagnosticSignError", { text = icons.Error, texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = icons.Warn, texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = icons.Info, texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = icons.Hint, texthl = "DiagnosticSignHint" })
+
+-- [[LSP Settings]]
+local border = "rounded"
+vim.diagnostic.config({
+	float = { border = border },
+})
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+	border = border,
+})
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+	border = border,
+})
+
+return M
