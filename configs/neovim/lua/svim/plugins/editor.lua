e1366241bd7c874cbf868daf9ee826e625c8fe01
Martin Schenck <martin.schenck@afilio.de>
2024-06-13 12:07:10 +0200

    Nvim: Add lualine

--- configs/neovim/lua/svim/plugins/editor.lua
+++ configs/neovim/lua/svim/plugins/editor.lua
@@ -365,4 +365,35 @@ return {
     event = "InsertEnter",
     config = true,
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      options = { theme = "catppuccin" },
+      disabled_filetypes = {
+        statusline = {
+          "aerial",
+          "NeogitCommitPopup",
+          "NeogitStatus",
+        },
+        winbar = {},
+      },
+      sections = {
+        lualine_a = { "mode", "searchcount" },
+        lualine_b = { "diff", "diagnostics" },
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+    },
+  },
 }
