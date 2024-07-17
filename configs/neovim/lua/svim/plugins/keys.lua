e238811d5d7a71b0e52c84ae3130b893d1ccda75
Martin Schenck <martin.schenck@afilio.de>
2024-07-17 09:09:07 +0200

    Nvim: Update packages

--- configs/neovim/lua/svim/plugins/keys.lua
+++ configs/neovim/lua/svim/plugins/keys.lua
@@ -1,26 +1,33 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
   config = function()
     local wk = require("which-key")
-    wk.register({
-      ["<c-w>"] = {
-        name = "Window",
-        s = { "<cmd>vsplit<cr>", "Split window" },
-        v = { "<cmd>split<cr>", "Split window verticallly" },
+    wk.setup({
+      icons = {
+        mappings = false,
       },
-      ["<leader>"] = {
-        name = "Leader",
-        g = "Git",
-        l = {
-          name = "Code",
-
-          a = { vim.lsp.buf.code_action, "Code actions" },
-          e = { vim.diagnostic.open_float, "Floating diagnostic" },
-          r = { vim.lsp.buf.rename, "Rename" },
-        },
-        t = "Todos",
-      },
+    })
+    wk.add({
+      { "<c-w>", group = "Window" },
+      { "<c-w>s", "<cmd>vsplit<cr>", desc = "Split window" },
+      { "<c-w>v", "<cmd>split<cr>", desc = "Split window verticallly" },
+      { "<leader>", group = "Leader" },
+      { "<leader>g", desc = "Git" },
+      { "<leader>l", group = "Code" },
+      { "<leader>la", vim.lsp.buf.code_action, desc = "Code actions" },
+      { "<leader>le", vim.diagnostic.open_float, desc = "Floating diagnostic" },
+      { "<leader>lr", vim.lsp.buf.rename, desc = "Rename" },
+      { "<leader>t", desc = "Todos" },
     })
   end,
 }
