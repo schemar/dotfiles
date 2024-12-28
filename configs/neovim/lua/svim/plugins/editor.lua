38bf10a2463f12b665dd9c4da3e2dd72b16e2695
Martin Schenck <martinschenck@fastmail.com>
2024-12-28 20:40:08 +0100

    tmux: Navigate panes now with modifier-hjkl instead of control

--- configs/neovim/lua/svim/plugins/editor.lua
+++ configs/neovim/lua/svim/plugins/editor.lua
@@ -223,6 +223,24 @@ return {
   {
     "christoomey/vim-tmux-navigator", -- Switch windows/panes vim/tmux
     event = { "VeryLazy" },
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1
+    end,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<m-h>", "<cmd>TmuxNavigateLeft<cr>" },
+      { "<m-j>", "<cmd>TmuxNavigateDown<cr>" },
+      { "<m-k>", "<cmd>TmuxNavigateUp<cr>" },
+      { "<m-l>", "<cmd>TmuxNavigateRight<cr>" },
+      { "<m-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+    },
   },
   {
     "lukas-reineke/indent-blankline.nvim",
