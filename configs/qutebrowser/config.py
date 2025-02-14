44856907f5f8e7cefcb0b08bc8ac98bf16fd82bd
Martin Schenck <martin.schenck@afilio.de>
2025-02-14 12:28:47 +0100

    qutebrowser: add config

---
+++ configs/qutebrowser/config.py
@@ -0,0 +1,8 @@
+import catppuccin
+
+# set the flavor you'd like to use
+# valid options are 'mocha', 'macchiato', 'frappe', and 'latte'
+# last argument (optional, default is False): enable the plain look for the menu rows
+catppuccin.setup(c, 'macchiato', False)
+
+c.window.hide_decoration = True
