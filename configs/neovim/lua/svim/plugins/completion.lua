be3f09ea678e3be3d1447916d8d47cf74b1133b3
Martin Schenck <martinschenck@fastmail.com>
2025-02-16 22:44:44 +0100

    nvim(cmp): add border to windows
    https://github.com/LazyVim/LazyVim/discussions/5056

--- configs/neovim/lua/svim/plugins/completion.lua
+++ configs/neovim/lua/svim/plugins/completion.lua
@@ -44,6 +44,18 @@ return {
         nerd_font_variant = "mono",
       },
 
+      completion = {
+        menu = {
+          border = "rounded",
+          winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+        },
+        documentation = {
+          window = {
+            border = "rounded",
+          },
+        },
+      },
+
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
