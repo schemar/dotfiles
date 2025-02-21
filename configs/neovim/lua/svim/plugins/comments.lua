1f80b60b317fcdfecb52b253157d31f5577261d0
Martin Schenck <martinschenck@fastmail.com>
2025-02-21 09:50:12 +0100

    nvim(todo): move todo comments into comments module

--- configs/neovim/lua/svim/plugins/comments.lua
+++ configs/neovim/lua/svim/plugins/comments.lua
@@ -8,4 +8,40 @@ return {
       })
     end,
   },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    event = { "VeryLazy" },
+    keys = {
+      {
+        "]t",
+        function()
+          require("todo-comments").jump_next()
+        end,
+        desc = "Next todo comment",
+      },
+      {
+        "[t",
+        function()
+          require("todo-comments").jump_prev()
+        end,
+        desc = "Previous todo comment",
+      },
+    },
+    opts = {
+      keywords = {
+        TODOMS = { icon = " ", color = "info" },
+      },
+      highlight = {
+        -- Remove required trailing colon:
+        pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
+      },
+      search = {
+        -- regex that will be used to match keywords.
+        -- don't replace the (KEYWORDS) placeholder
+        -- pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+        pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+      },
+    },
+  },
 }
--- configs/neovim/lua/svim/plugins/editor.lua
+++ configs/neovim/lua/svim/plugins/editor.lua
@@ -58,42 +58,6 @@ return {
     },
   },
   {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    event = { "VeryLazy" },
-    keys = {
-      {
-        "]t",
-        function()
-          require("todo-comments").jump_next()
-        end,
-        desc = "Next todo comment",
-      },
-      {
-        "[t",
-        function()
-          require("todo-comments").jump_prev()
-        end,
-        desc = "Previous todo comment",
-      },
-    },
-    opts = {
-      keywords = {
-        TODOMS = { icon = " ", color = "info" },
-      },
-      highlight = {
-        -- Remove required trailing colon:
-        pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
-      },
-      search = {
-        -- regex that will be used to match keywords.
-        -- don't replace the (KEYWORDS) placeholder
-        -- pattern = [[\b(KEYWORDS):]], -- ripgrep regex
-        pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
-      },
-    },
-  },
-  {
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
     dependencies = {
