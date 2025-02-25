19e759b0afea3aff4fff2271aaab075a84831167
Martin Schenck <martinschenck@fastmail.com>
2025-02-25 18:29:31 +0100

    nevim(other): add temporal test files

--- configs/neovim/lua/svim/plugins/editor.lua
+++ configs/neovim/lua/svim/plugins/editor.lua
@@ -256,16 +256,76 @@ return {
       -- Map files to other files.
       -- See documentation for details and more options.
       mappings = {
-        { pattern = "(.*)/(.*).ts", target = "%1/%2.vue", context = "vue" },
-        { pattern = "(.*)/(.*).vue", target = "%1/%2.ts", context = "vue" },
-        { pattern = "(.*)/(.*).vue.gen.ts", target = "%1/%2.vue", context = "gen" },
-        { pattern = "(.*)/(.*).vue", target = "%1/%2.vue.gen.ts", context = "gen" },
-        { pattern = "(.*)/(.*).ts", target = "%1/%2.spec.ts", context = "spec" },
-        { pattern = "(.*)/(.*).spec.ts", target = "%1/%2.ts", context = "spec" },
-        { pattern = "(.*)/(.*).ts", target = "%1/%2.test.ts", context = "test" },
-        { pattern = "(.*)/(.*).test.ts", target = "%1/%2.ts", context = "test" },
-        { pattern = "(.*)/(.*).ts", target = "%1/%2.unit.test.ts", context = "test" },
-        { pattern = "(.*)/(.*).unit.test.ts", target = "%1/%2.ts", context = "test" },
+        {
+          pattern = "(.*)/(.*).ts",
+          target = "%1/%2.vue",
+          context = "vue",
+        },
+        {
+          pattern = "(.*)/(.*).vue",
+          target = "%1/%2.ts",
+          context = "vue",
+        },
+        {
+          pattern = "(.*)/(.*).vue.gen.ts",
+          target = "%1/%2.vue",
+          context = "gen",
+        },
+        {
+          pattern = "(.*)/(.*).vue",
+          target = "%1/%2.vue.gen.ts",
+          context = "gen",
+        },
+        {
+          pattern = "(.*)/(.*).ts",
+          target = "%1/%2.spec.ts",
+          context = "spec",
+        },
+        {
+          pattern = "(.*)/(.*).spec.ts",
+          target = "%1/%2.ts",
+          context = "spec",
+        },
+        {
+          pattern = "(.*)/(.*).ts",
+          target = "%1/%2.test.ts",
+          context = "test",
+        },
+        {
+          pattern = "(.*)/(.*).test.ts",
+          target = "%1/%2.ts",
+          context = "test",
+        },
+        {
+          pattern = "(.*)/(.*).ts",
+          target = "%1/%2.unit.test.ts",
+          context = "test",
+        },
+        {
+          pattern = "(.*)/(.*).unit.test.ts",
+          target = "%1/%2.ts",
+          context = "test",
+        },
+        {
+          pattern = "(.*)/(.*).ts",
+          target = "%1/%2.firestore.test.ts",
+          context = "test",
+        },
+        {
+          pattern = "(.*)/(.*).firestore.test.ts",
+          target = "%1/%2.ts",
+          context = "test",
+        },
+        {
+          pattern = "(.*)/(.*).ts",
+          target = "%1/%2.temporal.test.ts",
+          context = "test",
+        },
+        {
+          pattern = "(.*)/(.*).temporal.test.ts",
+          target = "%1/%2.ts",
+          context = "test",
+        },
       },
     },
   },
