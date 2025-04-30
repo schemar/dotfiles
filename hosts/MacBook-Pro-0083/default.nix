9d6d61b22f77fc99284c26726feb4da178eaab6b
Martin Schenck <martinschenck@fastmail.com>
2025-04-30 09:24:06 +0200

    nix(mbp): add graphite to brew packages for host mbp

--- hosts/MacBook-Pro-0083/default.nix
+++ hosts/MacBook-Pro-0083/default.nix
@@ -2,4 +2,10 @@
   imports = [
     ../common/homebrew.nix
   ];
+
+  # Additional brew settings for this host only
+  homebrew = {
+    taps = [ "withgraphite/tap" ];
+    brews = [ "withgraphite/tap/graphite" ];
+  };
 }
