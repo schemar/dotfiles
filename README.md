3b8aba6954367ccf10a6ca81da62117531267152
Martin Schenck <martinschenck@fastmail.com>
2025-05-02 13:15:53 +0200

    cleanup: remove unused configs

--- README.md
+++ README.md
@@ -9,15 +9,10 @@ This repository contains configurations for my productivity stack and some basic
 - [bat](https://github.com/sharkdp/bat)
 - [bottom](https://github.com/ClementTsang/bottom)
 - [fzf](https://github.com/junegunn/fzf)
-- [gitui](https://github.com/extrawurst/gitui)
-- [helix](https://github.com/helix-editor/helix)
 - [lsd](https://github.com/lsd-rs/lsd)
-- [mise](https://github.com/jdx/mise)
 - [NeoVim](https://github.com/neovim/neovim)❤️
-- [ranger](https://github.com/ranger/ranger)
 - [starship](https://github.com/starship/starship)
 - [tmux](https://github.com/tmux/tmux)❤️
-- [yazi](https://github.com/sxyazi/yazi)
 - [Zsh](https://www.zsh.org/)
 
 ### Graphical System
--- users/schemar/default.nix
+++ users/schemar/default.nix
@@ -22,53 +22,6 @@
 
   imports = [
     ../../configs/starship
+    ../../configs/zsh
   ];
-
-  programs.zsh = {
-    enable = true;
-
-    defaultKeymap = "viins";
-
-    shellAliases = {
-      g = "git";
-      y = "yarn";
-
-      l = "lsd -al";
-      ll = "lsd -al --tree";
-
-      h = "function hdi(){ howdoi $* -c -n 5; }; hdi";
-
-      n = "nvim";
-      ng = "rm -f /Users/schemar/.cache/godothost && nvim --listen /Users/schemar/.cache/godothost";
-
-      # Required for gitui to work with git push via SSH:
-      gitui = "eval $(ssh-agent) && ssh-add ~/.ssh/id_rsa && gitui";
-
-      fkill = "ps -efl | fzf | awk '\''{print $4}'\'' | xargs kill";
-
-      # Tmux usability aliases
-      tma = "tmux new-session -A -s afilio";
-      tmd = "tmux new-session -A -s dots";
-      tme = "tmux new-session -A -s eoi";
-      tmh = "tmux new-session -A -s home-as";
-
-      # Enable 256 colors in tmux
-      tmux = "tmux -2";
-    };
-
-    initExtra = ''
-      ${builtins.readFile ../../configs/zsh/.zshrc}
-    '';
-  };
-
-  programs.direnv = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-
-  programs.zoxide = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-
 }
