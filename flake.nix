a63c1f0c9568ffe4e7d0e320e0ca1684fe579c34
Martin Schenck <martinschenck@fastmail.com>
2025-06-06 21:22:23 +0200

    nixvim: use nixos-25.05 branch

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nixvim.url = "github:nix-community/nixvim";
+    nixvim.url = "github:nix-community/nixvim/nixos-25.05";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
 
     blueberry-peach.url = "github:schemar/blueberry-peach";
