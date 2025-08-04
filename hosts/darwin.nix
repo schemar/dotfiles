58dd8bb10700be7f991e65e8982ecaf196351024
Martin Schenck <martinschenck@fastmail.com>
2025-08-04 19:02:00 +0200

    nix: move common settings into existing shared modules

--- hosts/darwin.nix
+++ hosts/darwin.nix
@@ -3,6 +3,10 @@
   # The platform the configuration will be used on:
   nixpkgs.hostPlatform = "aarch64-darwin";
 
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 5;
+
   # Required by home-manager:
   users.users.schemar.home = /Users/schemar;
 
