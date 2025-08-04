1306bea3fe661c32ebcf25680ff1267b43d8d924
Martin Schenck <martinschenck@fastmail.com>
2025-08-04 19:04:50 +0200

    klabautermann: add schemar's ssh key

--- hosts/klabautermann/default.nix
+++ hosts/klabautermann/default.nix
@@ -2,6 +2,10 @@
 {
   nixpkgs.hostPlatform = "aarch64-linux";
 
+  users.users.schemar.openssh.authorizedKeys.keys = [
+    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK/4l6M0rsHmCuYHkUxuk42+gKN/tySO9CRLp0NOUjuH''
+  ];
+
   imports = [
     ./configuration.nix
     ../common.nix
