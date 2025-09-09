a0246021f124c320261599d6bd3da12f183bcdcb
Martin Schenck <martinschenck@fastmail.com>
2025-09-09 09:16:48 +0200

    afilio-host: add imagemagick and ghostscript brews

--- hosts/Afilio-0083/default.nix
+++ hosts/Afilio-0083/default.nix
@@ -19,6 +19,10 @@
       "podman"
       "pulumi/tap/pulumi"
       "withgraphite/tap/graphite"
+
+      # For PDF shrinking:
+      "imagemagick"
+      "ghostscript"
     ];
     casks = [
       "polypane"
