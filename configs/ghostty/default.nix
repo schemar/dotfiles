dfb909c6761ef73c9507122febec41bba6ecdeca
Martin Schenck <martinschenck@fastmail.com>
2025-11-05 15:39:46 +0100

    configs(ghostty): switch theme from file to external theme package input

--- configs/ghostty/default.nix
+++ configs/ghostty/default.nix
@@ -4,16 +4,9 @@
     source = ./config;
   };
   xdg.configFile."ghostty/themes/blueberry_peach_dark" = {
-    source = ./themes/blueberry_peach_dark;
+    source = "${inputs.blueberry-peach}/ports/ghostty/blueberry_peach_dark";
   };
   xdg.configFile."ghostty/themes/blueberry_peach_light" = {
-    source = ./themes/blueberry_peach_light;
+    source = "${inputs.blueberry-peach}/ports/ghostty/blueberry_peach_light";
   };
-  # TODO: Move these to blueberry peach package:
-  # xdg.configFile."ghostty/themes/blueberry_peach_dark" = {
-  #   source = "${inputs.blueberry-peach}/ports/ghostty/blueberry_peach_dark";
-  # };
-  # xdg.configFile."ghostty/themes/blueberry_peach_light" = {
-  #   source = "${inputs.blueberry-peach}/ports/ghostty/blueberry_peach_light";
-  # };
 }
