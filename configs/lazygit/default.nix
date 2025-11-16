9c5003314ad49f5fd2176a76a64704808d8450d6
Martin Schenck <martinschenck@fastmail.com>
2025-11-16 22:06:26 +0100

    configs(lazygit): advance config

--- configs/lazygit/default.nix
+++ configs/lazygit/default.nix
@@ -3,6 +3,22 @@
   programs.lazygit = {
     enable = true;
     settings = {
+      gui = {
+        scrollHeight = 5;
+        scrollOffMargin = 5;
+
+        showNumstatInFilesView = true;
+
+        border = "single";
+        filterMode = "fuzzy";
+      };
+      git = {
+        paging = {
+          # Same themeing as in git delta config. Theme comes from bat.
+          pager = "delta --paging=never --line-numbers --features syntax-theme-`$HOME/.config/current_theme`";
+          colorArg = "always";
+        };
+      };
       customCommands = [
         {
           key = "<c-/>";
