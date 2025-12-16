64166d370b58df422b4593715378660a9aa09a4a
Martin Schenck <martinschenck@fastmail.com>
2025-12-16 14:23:15 +0100

    configs(neovim): remove code actions override for typescript
    VTSLS provides regular code actions. VtsExec no longer exists.

--- configs/neovim/filetypes.nix
+++ configs/neovim/filetypes.nix
@@ -6,18 +6,5 @@
         expandtab = false;
       };
     };
-    "ftplugin/typescript.lua" = {
-      keymaps = [
-        {
-          action = "<cmd>VtsExec source_actions<cr>";
-          key = "<leader>la";
-          options = {
-            noremap = true;
-            silent = true;
-            desc = "Code actions";
-          };
-        }
-      ];
-    };
   };
 }
