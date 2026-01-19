d3799210cab6768f38257ec3b03978ac9a062020
Martin Schenck <martinschenck@fastmail.com>
2026-01-19 08:39:47 +0100

    configs(mise): default to node 24.11.1 globally

--- configs/mise/default.nix
+++ configs/mise/default.nix
@@ -4,8 +4,13 @@
     enable = true;
     enableZshIntegration = true;
 
-    globalConfig.settings = {
-      auto_install = true;
+    globalConfig = {
+      settings = {
+        auto_install = true;
+      };
+      tools = {
+        node = "24.11.1";
+      };
     };
   };
 }
