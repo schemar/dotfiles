80b8acee70a2e8539de6a89c13d1fc92ee709d8f
Martin Schenck <martinschenck@fastmail.com>
2026-01-23 21:11:03 +0100

    add ghostty

--- configs/ghostty/default.nix
+++ configs/ghostty/default.nix
@@ -1,8 +1,45 @@
-{ inputs, ... }:
 {
-  xdg.configFile."ghostty/config" = {
-    source = ./config;
-  };
+  inputs,
+  isDarwin,
+  ...
+}:
+{
+  xdg.configFile."ghostty/config".text = # ini{
+    ''
+      theme = dark:blueberry_peach_dark,light:blueberry_peach_light
+
+      font-family = Monaspace Neon
+      font-family = Symbols Nerd Font Mono
+
+      font-size = 13
+
+      font-feature = "calt=1"
+      font-feature = "dlig=1"
+      font-feature = "cv01=2"
+      font-feature = "ss01=1"
+      font-feature = "ss02=1"
+      font-feature = "ss03=1"
+      font-feature = "ss04=1"
+      font-feature = "ss05=1"
+      font-feature = "ss06=1"
+      font-feature = "ss07=1"
+      font-feature = "ss08=1"
+      font-feature = "ss09=1"
+      font-feature = "ss10=1"
+
+      quit-after-last-window-closed
+      confirm-close-surface = false
+
+      window-decoration = ${if isDarwin then "none" else "auto"}
+
+      window-padding-balance = true
+      window-padding-x = 3
+      window-padding-y = 1
+      window-padding-color = extend
+
+      macos-option-as-alt = true
+    '';
+
   xdg.configFile."ghostty/themes/blueberry_peach_dark" = {
     source = "${inputs.blueberry-peach}/ports/ghostty/blueberry_peach_dark";
   };
--- home/standalone.nix
+++ home/standalone.nix
@@ -1,13 +1,41 @@
-{ username, ... }:
+{
+  config,
+  pkgs,
+  username,
+  ...
+}:
 {
   # Standalone home-manager configuration for user
   # Use this for systems where home-manager is run standalone (not as a NixOS/nix-darwin module)
 
+  targets.genericLinux.enable = true;
+
+  home.file.".config/plasma-workspace/env/10-home-manager-xdg-data-dirs.sh" = {
+    executable = true;
+    text = ''
+      #!/bin/sh
+      export XDG_DATA_DIRS="${config.home.sessionVariables.XDG_DATA_DIRS}"
+    '';
+  };
+
+  systemd.user.sessionVariables = config.home.sessionVariables;
+
   home.username = username;
   home.homeDirectory = "/home/${username}";
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  programs = {
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
+  };
+
+  # Fonts
+  fonts.fontconfig.enable = true;
+  home.packages = with pkgs; [
+    ghostty
+
+    monaspace
+    nerd-fonts.symbols-only
+  ];
 
   imports = [
     ./default.nix
