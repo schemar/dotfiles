86fff16ce0d2ac0781e48cfa92bf1bbc92b621f7
Martin Schenck <martinschenck@fastmail.com>
2026-01-23 21:40:08 +0100

    ghostty: allow launching from the launcher

--- home/standalone.nix
+++ home/standalone.nix
@@ -37,6 +37,38 @@
     nerd-fonts.symbols-only
   ];
 
+  # Overwrite default desktop file to not have "DBusActivatable=true"
+  home.file.".local/share/applications/com.mitchellh.ghostty.desktop" = {
+    text = ''
+      [Desktop Entry]
+      Version=1.0
+      Name=Ghostty
+      Type=Application
+      Comment=A terminal emulator
+      TryExec=${pkgs.ghostty}/bin/ghostty
+      Exec=${pkgs.ghostty}/bin/ghostty --gtk-single-instance=true
+      Icon=com.mitchellh.ghostty
+      Categories=System;TerminalEmulator;
+      Keywords=terminal;tty;pty;
+      StartupNotify=true
+      StartupWMClass=com.mitchellh.ghostty
+      Terminal=false
+      Actions=new-window;
+      X-GNOME-UsesNotifications=true
+      X-TerminalArgExec=-e
+      X-TerminalArgTitle=--title=
+      X-TerminalArgAppId=--class=
+      X-TerminalArgDir=--working-directory=
+      X-TerminalArgHold=--wait-after-command
+      X-KDE-Shortcuts=Ctrl+Alt+T
+
+      [Desktop Action new-window]
+      Name=New Window
+      Exec=/nix/store/4fkwvnja07zmivbi0l68351rzd3166hp-ghostty-1.2.3/bin/ghostty --gtk-single-instance=true
+
+    '';
+  };
+
   imports = [
     ./default.nix
   ];
