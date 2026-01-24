d6bcc0ee540c8f57ed824212dcc81b440fa397bd
Martin Schenck <martinschenck@fastmail.com>
2026-01-24 22:07:34 +0100

    tmux: move extra config to correct block

--- configs/tmux/default.nix
+++ configs/tmux/default.nix
@@ -39,9 +39,6 @@
         };
         extraConfig = # tmux
           ''
-            # Ensure tmux jobs/plugins can find basic tools and bash
-            set-environment -g PATH "$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:/usr/local/bin:/usr/bin:/bin"
-
             source ${./config/reset_catppuccin.conf}
             if-shell '[ "$(~/.config/current_theme)" = "dark" ]' \
               "source-file ${inputs.blueberry-peach}/ports/tmux/blueberry_peach_dark.conf" \
@@ -144,6 +141,9 @@
 
     extraConfig = # tmux
       ''
+        # Ensure tmux jobs/plugins can find basic tools and bash
+        set-environment -g PATH "$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:/usr/local/bin:/usr/bin:/bin"
+
         # Colors
         # https://gist.github.com/andersevenrud/015e61af2fd264371032763d4ed965b6
         set -sg terminal-overrides ",*:RGB"
