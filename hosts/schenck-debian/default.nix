2315803af48079c7ab384c336ea017f6e82cc64a
Martin Schenck <martinschenck@fastmail.com>
2026-01-30 21:17:20 +0100

    sway: enable screensharing

--- hosts/schenck-debian/default.nix
+++ hosts/schenck-debian/default.nix
@@ -286,6 +286,10 @@
     libnotify
     wl-clipboard
 
+    xdg-desktop-portal-wlr
+    slurp
+    grim
+
     networkmanager
     networkmanagerapplet
 
@@ -294,6 +298,23 @@
 
     ghostty
   ];
+
+  home.file.".config/systemd/user/xdg-desktop-portal-wlr.service".text = # ini
+    ''
+      [Unit]
+      Description=xdg-desktop-portal-wlr
+      PartOf=graphical-session.target
+      After=graphical-session.target
+
+      [Service]
+      Type=dbus
+      BusName=org.freedesktop.impl.portal.desktop.wlr
+      ExecStart=${pkgs.xdg-desktop-portal-wlr}/libexec/xdg-desktop-portal-wlr
+      Restart=on-failure
+
+      [Install]
+      WantedBy=graphical-session.target
+    '';
 
   # Overwrite default desktop file to not have "DBusActivatable=true"
   home.file.".local/share/applications/com.mitchellh.ghostty.desktop" = {
