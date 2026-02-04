18e25f56b24672eca1c1143a6fcaa8270c0bc28b
Martin Schenck <martinschenck@fastmail.com>
2026-02-04 21:10:12 +0100

    configs(mako): basic styling

--- configs/mako/default.nix
+++ configs/mako/default.nix
@@ -4,8 +4,16 @@
     enable = true;
 
     settings = {
-      default-timeout = 10000; # 10 seconds
       font = "Monaspace Neon 11";
+      background-color = "#0B0A0F";
+      text-color = "#A2A2A9";
+      width = "600";
+      height = "80";
+      border-size = "2";
+      border-color = "#A19DD4FF";
+      border-radius = "8";
+      progress-color = "over #A19DD4FF";
+      padding = "8";
     };
   };
 
