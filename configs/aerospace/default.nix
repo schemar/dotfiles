0230b600058b7d57bb972f4ebda153dd6974c654
Martin Schenck <martinschenck@fastmail.com>
2026-06-29 20:39:37 +0200

    configs(aerospace): try more ergonomic keys

--- configs/aerospace/default.nix
+++ configs/aerospace/default.nix
@@ -16,48 +16,100 @@
       };
 
       mode.main.binding = {
-        ctrl-alt-h = "focus left";
-        ctrl-alt-j = "focus down";
-        ctrl-alt-k = "focus up";
-        ctrl-alt-l = "focus right";
+        alt-1 = "workspace 1";
+        alt-2 = "workspace 2";
+        alt-3 = "workspace 3";
+        alt-4 = "workspace 4";
+        alt-5 = "workspace 5";
+        alt-6 = "workspace 6";
+        alt-7 = "workspace 7";
+        alt-8 = "workspace 8";
+        alt-9 = "workspace 9";
+        alt-0 = "workspace 10";
 
-        ctrl-alt-shift-h = "move left";
-        ctrl-alt-shift-j = "move down";
-        ctrl-alt-shift-k = "move up";
-        ctrl-alt-shift-l = "move right";
-
-        ctrl-alt-cmd-shift-h = "join-with left";
-        ctrl-alt-cmd-shift-j = "join-with down";
-        ctrl-alt-cmd-shift-k = "join-with up";
-        ctrl-alt-cmd-shift-l = "join-with right";
-
-        ctrl-alt-1 = "workspace 1";
-        ctrl-alt-2 = "workspace 2";
-        ctrl-alt-3 = "workspace 3";
-        ctrl-alt-4 = "workspace 4";
-        ctrl-alt-5 = "workspace 5";
-        ctrl-alt-6 = "workspace 6";
-        ctrl-alt-7 = "workspace 7";
-        ctrl-alt-8 = "workspace 8";
-        ctrl-alt-9 = "workspace 9";
-        ctrl-alt-0 = "workspace 10";
-
-        ctrl-alt-shift-1 = "move-node-to-workspace 1";
-        ctrl-alt-shift-2 = "move-node-to-workspace 2";
-        ctrl-alt-shift-3 = "move-node-to-workspace 3";
-        ctrl-alt-shift-4 = "move-node-to-workspace 4";
-        ctrl-alt-shift-5 = "move-node-to-workspace 5";
-        ctrl-alt-shift-6 = "move-node-to-workspace 6";
-        ctrl-alt-shift-7 = "move-node-to-workspace 7";
-        ctrl-alt-shift-8 = "move-node-to-workspace 8";
-        ctrl-alt-shift-9 = "move-node-to-workspace 9";
-        ctrl-alt-shift-0 = "move-node-to-workspace 10";
+        alt-shift-1 = "move-node-to-workspace 1";
+        alt-shift-2 = "move-node-to-workspace 2";
+        alt-shift-3 = "move-node-to-workspace 3";
+        alt-shift-4 = "move-node-to-workspace 4";
+        alt-shift-5 = "move-node-to-workspace 5";
+        alt-shift-6 = "move-node-to-workspace 6";
+        alt-shift-7 = "move-node-to-workspace 7";
+        alt-shift-8 = "move-node-to-workspace 8";
+        alt-shift-9 = "move-node-to-workspace 9";
+        alt-shift-0 = "move-node-to-workspace 10";
 
         alt-tab = "workspace-back-and-forth";
+        alt-semicolon = "mode advanced";
+      };
 
-        ctrl-alt-semicolon = "mode service";
-        ctrl-alt-r = "mode resize";
-        ctrl-alt-space = "layout floating tiling"; # Toggle between floating and tiling
+      mode.advanced.binding = {
+        esc = "mode main";
+
+        h = [
+          "focus left"
+          "mode main"
+        ];
+        j = [
+          "focus down"
+          "mode main"
+        ];
+        k = [
+          "focus up"
+          "mode main"
+        ];
+        l = [
+          "focus right"
+          "mode main"
+        ];
+
+        shift-h = [
+          "move left"
+          "mode main"
+        ];
+        shift-j = [
+          "move down"
+          "mode main"
+        ];
+        shift-k = [
+          "move up"
+          "mode main"
+        ];
+        shift-l = [
+          "join-with right"
+          "mode main"
+        ];
+        alt-shift-h = [
+          "join-with left"
+          "mode main"
+        ];
+        alt-shift-j = [
+          "join-with down"
+          "mode main"
+        ];
+        alt-shift-k = [
+          "join-with up"
+          "mode main"
+        ];
+        alt-shift-l = [
+          "join-with right"
+          "mode main"
+        ];
+
+        r = "mode resize";
+        space = "layout floating tiling"; # Toggle between floating and tiling
+
+        c = [
+          "reload-config"
+          "mode main"
+        ];
+        f = [
+          "flatten-workspace-tree"
+          "mode main"
+        ]; # reset layout
+        backspace = [
+          "close-all-windows-but-current"
+          "mode main"
+        ];
       };
 
       mode.resize.binding = {
@@ -68,21 +120,6 @@
         j = "resize height -50";
         k = "resize height +50";
         l = "resize width +50";
-      };
-
-      mode.service.binding = {
-        esc = [
-          "reload-config"
-          "mode main"
-        ];
-        r = [
-          "flatten-workspace-tree"
-          "mode main"
-        ]; # reset layout
-        backspace = [
-          "close-all-windows-but-current"
-          "mode main"
-        ];
       };
 
       # Cannot auto-move Todoist as we cannot differentiate between the main app window and the quick entry pop-up.
