c07bc5e886eb8de40e8bda8f3029bcd5f2f62dd7
Martin Schenck <martinschenck@fastmail.com>
2026-06-30 08:47:48 +0200

    hosts(nb0407): remove unnecessary graphics compute package

--- hosts/nb0407/default.nix
+++ hosts/nb0407/default.nix
@@ -52,9 +52,6 @@
       # Required for modern Intel GPUs (Xe iGPU and ARC)
       intel-media-driver # VA-API (iHD) userspace
       vpl-gpu-rt # oneVPL (QSV) runtime
-
-      # Optional (compute / tooling):
-      intel-compute-runtime # OpenCL (NEO) + Level Zero for Arc/Xe
     ];
   };
   environment.sessionVariables = {
