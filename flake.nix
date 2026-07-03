93fe38eb14a896cc1cac1122f7e7e9bed87212e7
Martin Schenck <martinschenck@fastmail.com>
2026-07-03 14:12:07 +0200

    hosts(nb0400): delete

--- flake.nix
+++ flake.nix
@@ -93,19 +93,6 @@
             ./hosts/nb0407
           ];
         };
-        "${username}@nb0400" = home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs {
-            system = "x86_64-linux";
-            config.allowUnfree = true;
-          };
-          extraSpecialArgs = {
-            inherit inputs username;
-            isDarwin = false;
-          };
-          modules = [
-            ./hosts/nb0400
-          ];
-        };
       };
     };
 }
