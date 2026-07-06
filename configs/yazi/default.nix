b8bac12115339d9fde02d15b8f09e9870e87c2ff
Martin Schenck <martinschenck@fastmail.com>
2026-07-06 22:00:17 +0200

    configs(yazi): add blueberry-peach theme

--- configs/yazi/default.nix
+++ configs/yazi/default.nix
@@ -1,5 +1,15 @@
-{ ... }:
+{ inputs, ... }:
 {
+  xdg.configFile."yazi/theme.toml" = {
+    source = "${inputs.blueberry-peach}/ports/yazi/theme.toml";
+  };
+  xdg.configFile."yazi/flavors/blueberry-peach-dark.yazi" = {
+    source = "${inputs.blueberry-peach}/ports/yazi/blueberry-peach-dark.yazi";
+  };
+  xdg.configFile."yazi/flavors/blueberry-peach-light.yazi" = {
+    source = "${inputs.blueberry-peach}/ports/yazi/blueberry-peach-light.yazi";
+  };
+
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
