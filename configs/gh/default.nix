7706ca4ba477de4c682e0cf46dbcf97b896d7232
Martin Schenck <martinschenck@fastmail.com>
2026-07-20 11:56:14 +0200

    configs(gh): expand with `gh stack` extension (incl. aliases)

--- configs/gh/default.nix
+++ configs/gh/default.nix
@@ -1,6 +1,31 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  programs.zsh.shellAliases.h = "gh";
+
   programs.gh = {
     enable = true;
+    extensions = with pkgs; [
+      gh-stack
+    ];
+    settings = {
+      git_protocol = "ssh";
+      aliases = {
+        s = "stack";
+        sa = "stack add";
+        sb = "stack bottom";
+        sd = "stack down";
+        sh = "stack push";
+        so = "stack checkout";
+        si = "stack init";
+        sm = "stack modify";
+        ss = "stack sync";
+        st = "stack top";
+        sp = "stack submit";
+        sr = "stack rebase";
+        su = "stack up";
+        sv = "stack view";
+        sw = "stack switch";
+      };
+    };
   };
 }
