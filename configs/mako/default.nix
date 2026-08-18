ecde51ccd1d4a8f8ca477c045c8fe56500b3908a
Martin Schenck <martinschenck@fastmail.com>
2026-08-18 13:14:23 +0200

    configs(mako): read from blueberry-peach theme

--- configs/mako/default.nix
+++ configs/mako/default.nix
@@ -1,27 +1,21 @@
-{ ... }:
+{ inputs, ... }:
 {
   xdg.configFile."mako/config".text = # ini
     ''
       font=MonoLisaText 11
       width=600
       height=80
-      outer-margin=12,6,0,0
+      outer-margin=12,12,0,0
       margin=0,0,6,0
       border-size=1
       border-radius=0
       padding=8
 
       [mode=dark]
-      border-color=#A19DD4FF
-      background-color=#37363E
-      text-color=#A2A2A9
-      progress-color=over #A19DD4FF
+      ${builtins.readFile "${inputs.blueberry-peach}/ports/mako/blueberry_peach_dark"}
 
       [mode=light]
-      border-color=#6E3B96
-      background-color=#DDC8B6
-      text-color=#57534C
-      progress-color=over #6E3B96
+      ${builtins.readFile "${inputs.blueberry-peach}/ports/mako/blueberry_peach_light"}
     '';
 
   services.mako = {
