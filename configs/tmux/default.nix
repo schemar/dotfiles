442f1737c3cc3fd21b7d20fc8ff98ff1409f1d46
Martin Schenck <martinschenck@fastmail.com>
2026-08-25 13:39:06 +0200

    configs(tmux): update theme setting
    For some reason `-F` is worse.

--- configs/tmux/default.nix
+++ configs/tmux/default.nix
@@ -121,7 +121,7 @@
           "source-file ${inputs.blueberry-peach}/ports/tmux/blueberry_peach_dark.conf" \
           "source-file ${inputs.blueberry-peach}/ports/tmux/blueberry_peach_light.conf"
 
-        set -gF status-style "bg=#{@thm_surface_0},fg=#{@thm_text}"
+        set -g status-style "bg=#{@thm_surface_0},fg=#{@thm_text}"
 
         set -gF window-status-format "#[fg=#{@thm_surface_0},bg=#{@thm_dimmed}] ##I #[fg=#{@thm_text},bg=#{@thm_surface_0}] ##W ##F "
         set -gF window-status-current-format "#[fg=#{@thm_surface_0},bg=#{@thm_violet}] ##I #[fg=#{@thm_text},bg=#{@thm_surface_1}] ##W ##F "
@@ -129,16 +129,16 @@
         set -wgF pane-active-border-style "##{?pane_in_mode,fg=#{@thm_pink},##{?pane_synchronized,fg=#{@thm_violet},fg=#{@thm_pink}}}"
         set -wgF pane-border-style "fg=#{@thm_overlay_2}"
 
-        set -gF message-style "fg=#{@thm_teal},bg=#{@thm_surface_0},align=centre"
-        set -gF message-command-style "fg=#{@thm_teal},bg=#{@thm_surface_0},align=centre"
+        set -g message-style "fg=#{@thm_teal},bg=#{@thm_surface_0},align=centre"
+        set -g message-command-style "fg=#{@thm_teal},bg=#{@thm_surface_0},align=centre"
 
-        set -gF popup-style "bg=#{@thm_surface_0},fg=#{@thm_text}"
-        set -gF popup-border-style "fg=#{@thm_violet}"
+        set -g popup-style "bg=#{@thm_surface_0},fg=#{@thm_text}"
+        set -g popup-border-style "fg=#{@thm_violet}"
 
-        set -gF mode-style "bg=#{@thm_surface_1},bold"
-        set -gF clock-mode-colour "#{@thm_violet}"
+        set -g mode-style "bg=#{@thm_surface_1},bold"
+        set -g clock-mode-colour "#{@thm_violet}"
 
-        set -g status-left "#[bg=#{?client_prefix,blue,default},fg=#{?client_prefix,black,default}]#S#[default] "
+        set -gF status-left "#[bg=#{?client_prefix,blue,default},fg=#{?client_prefix,black,default}]#S#[default] "
         set -g status-right "#(${./config/pane_branch.sh})"
         # The branch name is often longer than the default length of 40.
         set -g status-right-length 70
