f272c48a41a456b77cf3ebbdc2aab2e779ef52af
Martin Schenck <martinschenck@fastmail.com>
2026-08-31 13:11:47 +0200

    config(helix): initial setup closer to nvim

--- configs/helix/default.nix
+++ configs/helix/default.nix
@@ -21,26 +21,61 @@
             v = "hsplit";
             C-v = "hsplit";
           };
-          space.w = {
-            s = "vsplit";
-            C-s = "vsplit";
-            v = "hsplit";
-            C-v = "hsplit";
+          space = {
+            # Yazi "integration":
+            C-f = [
+              ":sh rm -f /tmp/helix-yazi-selection-result"
+              ":insert-output yazi --chooser-file=/tmp/helix-yazi-selection-result"
+              ":sh printf \"\\x1b[?1049h\\x1b[?2004h\" > /dev/tty"
+              ":open %sh{cat /tmp/helix-yazi-selection-result}"
+              ":redraw"
+              # If both Helix and Yazi have mouse support enabled, they will
+              # conflict, you can use this trick to reset the mouse state when
+              # exiting Yazi and returning to Helix (https://github.com/sxyazi/yazi/pull/2461):
+              ":set mouse false"
+              ":set mouse true"
+            ];
+            # Lazygit "integration":
+            "C-g" = [
+              ":new"
+              ":insert-output lazygit"
+              ":buffer-close!"
+              ":redraw"
+            ];
+            w = {
+              s = "vsplit";
+              C-s = "vsplit";
+              v = "hsplit";
+              C-v = "hsplit";
+            };
           };
         };
       };
       editor = {
-        cursorline = true;
-        color-modes = true;
+        # Use system clipboard
+        default-yank-register = "+";
 
-        cursor-shape = {
-          insert = "bar";
-          normal = "block";
-          select = "underline";
-        };
+        # These may not get picked up properly in tmux
+        true-color = true;
+        undercurl = true;
+
+        rulers = [
+          80
+          120
+        ];
+
+        color-modes = true;
+        trim-trailing-whitespace = true;
+        popup-border = "all";
 
         indent-guides = {
           render = true;
+        };
+
+        end-of-line-diagnostics = "error";
+        inline-diagnostics = {
+          cursor-line = "info";
+          other-lines = "error";
         };
       };
     };
@@ -64,6 +99,15 @@
           formatter = {
             command = "${lib.getExe pkgs.erlfmt}";
             args = [ "-" ];
+          };
+        }
+      ];
+      grammar = [
+        {
+          name = "erlang";
+          source = {
+            git = "https://github.com/WhatsApp/tree-sitter-erlang";
+            rev = "67e7f7f05baf492ca2a7c0d9538761b242d33d95";
           };
         }
       ];
