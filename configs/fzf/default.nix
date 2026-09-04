2eab12f0cd49a7c6e4347bebdc2c357309a1d987
Martin Schenck <martinschenck@fastmail.com>
2026-09-04 08:28:20 +0200

    configs(fzf): remove outdated widget options

--- configs/fzf/default.nix
+++ configs/fzf/default.nix
@@ -4,9 +4,5 @@
     enable = true;
 
     defaultCommand = "fd --type f --hidden --exclude .git/";
-
-    fileWidgetOptions = [
-      "--preview 'bat --style=numbers,changes --color=always {} --theme=blueberry_peach_$(~/.config/current_theme)'"
-    ];
   };
 }
