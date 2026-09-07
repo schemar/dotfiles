346f70458cfc413682f637a033114dc7a2b8f1b4
Martin Schenck <martinschenck@fastmail.com>
2026-09-07 11:44:14 +0200

    configs(helix): update yaml tree-sitter grammar

--- configs/helix/languages/yaml.nix
+++ configs/helix/languages/yaml.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }: {
   programs.helix = {
     extraPackages = with pkgs; [
       yamlfmt
@@ -10,6 +10,19 @@
         {
           name = "yaml";
           auto-format = true;
+          formatter = {
+            command = lib.getExe pkgs.yamlfmt;
+            args = [ "-" ];
+          };
+        }
+      ];
+      grammar = [
+        {
+          name = "yaml";
+          source = {
+            git = "https://github.com/tree-sitter-grammars/tree-sitter-yaml";
+            rev = "7708026449bed86239b1cd5bce6e3c34dbca6415";
+          };
         }
       ];
     };
